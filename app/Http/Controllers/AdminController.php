7c11c6bc2ebe93152b55aeb8bdd2945ccae97e9e
wardahdurrahumniyyah <78292354+wardahdurrahumniyyah@users.noreply.github.com>
2021-04-06 09:19:37 +0700

    Tambah Update dan Delete

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use App\Models\Book;
 
 class AdminController extends Controller
@@ -48,4 +49,52 @@ class AdminController extends Controller
 
         return redirect()->route('admin.books')->with($notification);
     }
+
+public function update_book(Request $req)
+    {
+        $book = Book::find($req->get('id'));
+
+        $book->judul = $req->get('judul');
+        $book->penulis = $req->get('penulis');
+        $book->tahun = $req->get('tahun');
+        $book->penerbit = $req->get('penerbit');
+
+        if ($req->hasFile('cover')) {
+            $extension = $req->file('cover')->extension();
+
+            $filename = 'cover_buku' . time() . '.' . $extension;
+            $req->file('cover')->storeAs(
+                'public/cover_buku',
+                $filename
+            );
+            Storage::delete('public/cover_buku/'.$req->get('old_cover'));
+
+            $book->cover = $filename;
+        }
+        $book->save();
+
+        $notification = array(
+            'message' => 'Data buku berhasil diubah',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('admin.books')->with($notification);
+    }
+
+    public function delete_book(Request $req)
+    {
+        $book = Book::find($req->get('id'));
+
+        storage::delete('public/cover_buku/'.$req->get('old_cover'));
+
+        $book->delete();
+
+        $notification = array(
+            'message' => 'Data Buku Berhasil Dihapus',
+            'alert-type' => 'succes'
+        );
+
+        return redirect()->route('admin.books')->with($notification);
+    }
+
 }
